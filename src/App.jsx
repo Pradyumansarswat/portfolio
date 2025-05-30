9f584de91657ca7f9b3a7b57151df679752eed0c
Pradyuman Sarswat <pradyumansarswat@gmail.com>
2025-05-30 14:30:13 +0530

    uploading fronted code

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Experience from './components/Experience';
+import { Analytics } from '@vercel/analytics/react';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/experience' element={<Experience/>}/>
+      </Routes>
+      <Footer/>
+      <Analytics />
+    </BrowserRouter>
+      
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,56 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Full Stack Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Web & Tech Blogger",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Pradyuman';
+
+export const experiences = [
+  {
+    company: 'Decent Technologies',
+    role: 'Full Stack Developer',
+    duration: 'Dec 2023 - May 2025',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1m63QIQhfalQh8HwyGfFlqVdLZ5iIl-HUUD3HoyiJzKt8bDejSSihqUlvAutPHPPG3Ss&usqp=CAU',
+    points: [
+      'Developed scalable, high-performance web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js).',
+      'Designed and implemented responsive UIs with React.js and Tailwind CSS for seamless cross-device experiences.',
+      'Created and consumed RESTful APIs for efficient frontend-backend integration and smooth user interaction.',
+      'Handled backend logic, database operations, and server-side features to ensure application functionality and scalability.',
+      'Collaborated with cross-functional teams including UI/UX designers and project managers to deliver timely and quality software.',
+      'Used Git and GitHub for version control, team collaboration, and code reviews in agile development workflows.'
+    ],
+    url: 'https://decenttechnologies.in/',
+  },
+  {
+    company: 'Groot Software Solutions',
+    role: 'Full Stack Developer Intern',
+    duration: 'Jul 2023 - Nov 2023',
+    logo: 'https://media.licdn.com/dms/image/v2/C510BAQFlseDKJ1_P4g/company-logo_200_200/company-logo_200_200/0/1631432685337?e=2147483647&v=beta&t=SK1QXN4OXPewDp7RqIifLzMgCtU3UVffhUDwFv02l4Q',
+    points: [
+      'Gained hands-on experience in building real-world web apps using the MERN stack (MongoDB, Express.js, React.js, Node.js).',
+      'Built responsive UIs with Bootstrap and custom CSS, improving usability across devices.',
+      'Integrated RESTful APIs and managed application state using Redux and Redux Thunk.',
+      'Collaborated with senior developers and designers to implement scalable, maintainable features.',
+      'Strengthened version control practices using Git and GitHub in a team-based development setup.',
+      'Improved debugging and problem-solving skills by working in a fast-paced startup environment.'
+    ],
+    url: 'https://grootsoftware.com/',
+  }
+];
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
